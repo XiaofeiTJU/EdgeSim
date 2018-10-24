b286cad912d2fa7d9710a1920e7be24c924446a9
SeriousWenKai <liwenkai_1994@126.com>
2018-10-24 19:28:21 +0800

    Initial Version

--- src/main/java/cn/edu/tju/simulation/swing/chart/PythonInterpreter.java
+++ src/main/java/cn/edu/tju/simulation/swing/chart/PythonInterpreter.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 
 public class PythonInterpreter {
-	private static String path = PythonInterpreter.class.getClassLoader().getResource("").getPath().substring(1,PythonInterpreter.class.getClassLoader().getResource("").getPath().indexOf("EdgeSim"));
-	private static final String PY_HITRATRE_URL = path + "EdgeSim/src/main/java/cn/edu/tju/simulation/swing/chart/HitRate.py";
-	private static final String PY_LATENCY_URL = path + "EdgeSim/src/main/java/cn/edu/tju/simulation/swing/chart/Latency.py";
-	private static final String PY_TRAFFIC_URL = path + "EdgeSim/src/main/java/cn/edu/tju/simulation/swing/chart/Traffic.py";
+	private static String path = PythonInterpreter.class.getClassLoader().getResource("").getPath().substring(1,PythonInterpreter.class.getClassLoader().getResource("").getPath().indexOf("/out"));
+	private static final String PY_HITRATRE_URL = path + "/src/main/java/cn/edu/tju/simulation/swing/chart/HitRate.py";
+	private static final String PY_LATENCY_URL = path + "/src/main/java/cn/edu/tju/simulation/swing/chart/Latency.py";
+	private static final String PY_TRAFFIC_URL = path + "/src/main/java/cn/edu/tju/simulation/swing/chart/Traffic.py";
 
 	public static void interpreter(){
 		try {
+			System.out.println(PythonInterpreter.class.getClassLoader().getResource("").getPath());
+
 			Runtime.getRuntime().exec("cmd /k python "+ PY_HITRATRE_URL);
 			Runtime.getRuntime().exec("cmd /k python "+ PY_LATENCY_URL);
 			Runtime.getRuntime().exec("cmd /k python "+ PY_TRAFFIC_URL);
